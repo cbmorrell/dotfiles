614f29c865524504a0f862a7874c9843c8b1966c
Christian Morrell <cmorrell@unb.ca>
2026-01-16 14:19:43 -0400

    fix extend_table logic

--- config/wezterm/helpers/tables.lua
+++ config/wezterm/helpers/tables.lua
@@ -1,10 +1,17 @@
 local module = {}
 
-function module.extend_table(destination, source)
-    for key, value in pairs(source) do
-        destination[key] = value
+function module.extend_table(destination, new_data)
+  for key, value in pairs(new_data) do
+    if type(key) == "number" then
+      -- Handle array values (e.g., new_data = {{flag = true}})
+      table.insert(destination, value)
+    else
+      -- Handle map values (e.g., new_data = {flag = true})
+      destination[key] = value
     end
-    return destination
+  end
+
+  return destination
 end
 
 return module
