fb901d7b6a014c6203f87becc62645dfd2db86c3
Christian Morrell <cmorrell@unb.ca>
2026-02-03 15:21:47 -0400

    Add new themes

--- config/nvim/lua/plugins/themes.lua
+++ config/nvim/lua/plugins/themes.lua
@@ -44,5 +44,22 @@ return {
   },
   {
     'Mofiqul/vscode.nvim'
+  },
+  {
+    "vague-theme/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
+      })
+      vim.cmd("colorscheme vague")
+    end
+  },
+  {
+      "scottmckendry/cyberdream.nvim",
+      lazy = false,
+      priority = 1000,
   }
 }
