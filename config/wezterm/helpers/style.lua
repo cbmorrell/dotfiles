7e9fde8b78eb7b5018009e0650940d7dcde0192b
Christian Morrell <cmorrell@unb.ca>
2026-03-25 17:15:48 -0300

    change wezterm colorscheme to Shale

--- config/wezterm/helpers/style.lua
+++ config/wezterm/helpers/style.lua
@@ -4,7 +4,89 @@ local module = {}
 
 function module.setup(config)
   -- For example, changing the color scheme:
-  config.color_scheme = "Vs Code Dark+ (Gogh)"
+  config.color_schemes = {
+    ["Shale"] = {
+      -- Background / surfaces
+      background              = "#1e2124",
+      foreground              = "#c8cdd4",
+
+      -- Cursor
+      cursor_bg               = "#c8cdd4",
+      cursor_fg               = "#1e2124",
+      cursor_border           = "#c8cdd4",
+
+      -- Selection
+      selection_bg            = "#2e3438",
+      selection_fg            = "#c8cdd4",
+
+      -- Split pane divider
+      split                   = "#3a4046",
+
+      -- Scrollbar
+      scrollbar_thumb         = "#3a4046",
+
+      -- ANSI Colors (normal)
+      ansi = {
+        "#2e3438",  -- black   (color0)
+        "#c4756a",  -- red     (color1)
+        "#7ea87e",  -- green   (color2)
+        "#c9a96e",  -- yellow  (color3)
+        "#6d8fad",  -- blue    (color4)
+        "#9d86b5",  -- magenta (color5)
+        "#6aabaa",  -- cyan    (color6)
+        "#c8cdd4",  -- white   (color7)
+      },
+
+      -- ANSI Colors (bright)
+      brights = {
+        "#3a4046",  -- bright black   (color8)
+        "#d4877c",  -- bright red     (color9)
+        "#8fba8f",  -- bright green   (color10)
+        "#d9b97e",  -- bright yellow  (color11)
+        "#7ea0be",  -- bright blue    (color12)
+        "#ae98c6",  -- bright magenta (color13)
+        "#7bbcbb",  -- bright cyan    (color14)
+        "#dde1e6",  -- bright white   (color15)
+      },
+
+      -- Tab bar
+      tab_bar = {
+        background = "#181b1e",
+
+        active_tab = {
+          bg_color  = "#1e2124",
+          fg_color  = "#c8cdd4",
+          intensity = "Normal",
+          underline = "None",
+          italic    = false,
+          strikethrough = false,
+        },
+
+        inactive_tab = {
+          bg_color  = "#181b1e",
+          fg_color  = "#5a6370",
+        },
+
+        inactive_tab_hover = {
+          bg_color  = "#242830",
+          fg_color  = "#9aa3ae",
+          italic    = false,
+        },
+
+        new_tab = {
+          bg_color  = "#181b1e",
+          fg_color  = "#5a6370",
+        },
+
+        new_tab_hover = {
+          bg_color  = "#242830",
+          fg_color  = "#9aa3ae",
+          italic    = false,
+        },
+      },
+    },
+  }
+  config.color_scheme = "Shale"
   config.font = wezterm.font("MesloLGS Nerd Font")
 
   -- Use default hyperlink rules
